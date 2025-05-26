176e7a1a255288e87dca5a8cb089be9562839780
Tryd0g0lik <work80@mail.ru>
2025-05-26 15:24:18 +0700

    [dev]: complated

--- weather/models.py
+++ weather/models.py
@@ -2,15 +2,16 @@ from django.db import models
 from django.core.validators import (
     MaxLengthValidator,
     MinLengthValidator,
-    RegexValidator
+    RegexValidator,
 )
 from django.utils.translation import gettext_lazy as _
+
 # Create your models here.
+
 
 class WeatherForecast(models.Model):
     user_id = models.UUIDField(
         primary_key=True,
-        unique=True,
     )
     latitude = models.FloatField(
         max_length=7,
@@ -18,14 +19,12 @@ class WeatherForecast(models.Model):
         help_text=_("latitude for forecast"),
         validators=[
             MaxLengthValidator(
-                7,
-                message=_("Maximum quantity of symbols before 7 symbols/characters")
+                7, message=_("Maximum quantity of symbols before 7 symbols/characters")
             ),
             MinLengthValidator(
-                2,
-                message=_("Minimum quantity of symbols is 2 symbols/characters")
-            )
-        ]
+                2, message=_("Minimum quantity of symbols is 2 symbols/characters")
+            ),
+        ],
     )
     longitude = models.FloatField(
         max_length=7,
@@ -33,22 +32,21 @@ class WeatherForecast(models.Model):
         help_text=_("longitude for forecast"),
         validators=[
             MaxLengthValidator(
-                7,
-                message=_("Maximum quantity of symbols before 7 symbols/characters")
+                7, message=_("Maximum quantity of symbols before 7 symbols/characters")
             ),
             MinLengthValidator(
-                2,
-                message=_("Minimum quantity of symbols is 2 symbols/characters")
-            )
-        ]
+                2, message=_("Minimum quantity of symbols is 2 symbols/characters")
+            ),
+        ],
     )
     updated_at = models.DateTimeField(
         auto_now_add=True,
         help_text=_("Date when user is last time used the service"),
-        
     )
+
     def __str__(self):
         return "Latitude: %s. Longitude: %s" % (self.latitude, self.longitude)
+
     class Meta:
         db_table = "weather"
         verbose_name = _("weather")
